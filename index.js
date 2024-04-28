3016f0d16c2607aeb22ede238b39e6bf5280eca1
notHeisenberg007 <mahfuzantor2@gmail.com>
2024-04-28 15:26:28 +0600

    items added to database

--- index.js
+++ index.js
@@ -23,10 +23,28 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const craftCollection = client.db('textileArt').collection('crafts')
+
+        app.get('/crafts', async (req, res) => {
+            const cursor = craftCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.post('/crafts', async (req, res) => {
+            const newaCraft = req.body
+            const result = await craftCollection.insertOne(newaCraft)
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -35,10 +53,7 @@ async function run() {
         // await client.close();
     }
 }
-run().catch(console.dir);
-
-
-
+run().catch(console.dir)
 app.get('/', (req, res) => {
     res.send('Art & craft created')
 })
